ea80eb1bb34df7aaab99d96b59d02dd64d74d78b
Joseph Killelea <josephkillelea@gmail.com>
2024-08-15 18:11:05 -0500

    edit EDA

--- eda_app.py
+++ eda_app.py
@@ -2,31 +2,31 @@ import streamlit as st
 import pandas as pd
 
 
-@st.cache_data
-def load_data(df1):
-    return pd.read_pickle(df1)
+# @st.cache_data
+# def load_data(df1):
+#     return pd.read_pickle(df1)
 
-df1 = load_data('data/nfl_team_stats_2002_2023.pkl')
+# df1 = load_data('data/nfl_team_stats_2002_2023.pkl')
 
 def run_eda_app():
     st.subheader("Exploratory Data Analysis")
     submenu = st.sidebar.selectbox("Submenu", ["Descriptive", "Plots"])
-    if submenu == "Descriptive":
-        st.dataframe(df1)
+    # if submenu == "Descriptive":
+    #     st.dataframe(df1)
 
-        with st.expander("Data Types"):
-            st.dataframe(df1.dtypes)
+    #     with st.expander("Data Types"):
+    #         st.dataframe(df1.dtypes)
 
-        with st.expander("Data Summary"):
-            st.dataframe(df1.describe())
+    #     with st.expander("Data Summary"):
+    #         st.dataframe(df1.describe())
 
-    elif submenu == "Plots":
-        st.subheader("Plots for Our Data")
-        col1, col2 = st.columns([2, 1])
+    # elif submenu == "Plots":
+    #     st.subheader("Plots for Our Data")
+    #     col1, col2 = st.columns([2, 1])
 
-        with col1:
-            with st.bar_chart(df1):
-                pass
+    #     with col1:
+    #         with st.bar_chart(df1):
+    #             pass
             
         
 
